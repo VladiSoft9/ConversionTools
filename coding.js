77b235e679e28a10dba886d32e2b1052ae5105c9
Vlada991 <jovanovic.vlada16@gmail.com>
2025-01-07 15:40:14 +0100

    Version 1.1 - weight converter added

--- coding.js
+++ coding.js
@@ -7,8 +7,13 @@ const textBox2 = document.getElementById('textBox2')
 const toMiles = document.getElementById('toMiles')
 const toKm = document.getElementById('toKm')
 const result2 = document.getElementById('result2')
+const textBox3 = document.getElementById('textBox3')
+const toKG = document.getElementById('toKG')
+const toIB = document.getElementById('toIB')
+const result3 = document.getElementById('result3')
 let temp
 let distance
+let weight
 
 function convert(){
 
@@ -38,15 +43,36 @@ function convert2(){
         distance = textBox2.value
         distance = Number(distance)
         distance = distance/1.609344
-        result2.textContent = distance.toFixed(2) + 'mi'
+        result2.textContent = distance.toFixed(2) + ' mi'
     }
     else if(toKm.checked){
         distance = textBox2.value
         distance = Number(distance)
         distance = distance*1.609344
-        result2.textContent = distance.toFixed(2) + 'km'
+        result2.textContent = distance.toFixed(2) + ' km'
     }
     else{
         result2.textContent = 'Please check desired converting option!'
+    }
+}
+
+function convert3(){
+
+    if(toKG.checked){
+        weight = textBox3.value
+        weight = Number(weight)
+        weight = weight*0.45359
+        result3.textContent = weight.toFixed(2) + ' kg'
+    }
+
+    else if(toIB.checked){
+        weight = textBox3.value
+        weight = Number(weight)
+        weight = weight/0.45359
+        result3.textContent = weight.toFixed(2) + ' ib'
+    }
+
+    else{
+        result3.textContent = 'Please check desired converting option'
     }
 }
